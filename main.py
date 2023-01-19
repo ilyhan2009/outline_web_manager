dd410b810891f1770ec6e70a3ee7d57875990c8f
ilyhan2009 <orelcs@yandex.ru>
2023-01-19 22:43:43 +0300

    basic keys list page

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import typing as t
+
 import uvicorn
 
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, Cookie
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import starlette.status as status
+from outline_vpn.outline_vpn import OutlineVPN
 
 
 app = FastAPI()
@@ -14,44 +18,42 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
-    if True:
+async def root(request: Request, ApiUrl: t.Union[str, None] = Cookie(default=None)):
+    if not ApiUrl:
         return templates.TemplateResponse("sign-in.html", {"request": request})
 
-    return templates.TemplateResponse("main.html", {"request": request})
+    outline_client = OutlineVPN(api_url=ApiUrl)
+
+    data = {
+        'request': request,
+        'server_information': outline_client.get_server_information(),
+        'keys': outline_client.get_keys(),
+    }
+
+    return templates.TemplateResponse("main.html", data)
+
+
+@app.post("/sign-in", response_class=HTMLResponse)
+async def root(request: Request, ApiUrl: str = Form()):
+    response = RedirectResponse('/', status_code=status.HTTP_302_FOUND)
+
+    try:
+        outline_client = OutlineVPN(api_url=ApiUrl)
+
+        server_information = outline_client.get_server_information()
+    except Exception as e:
+        return response
+
+    response.set_cookie(key='ApiUrl', value=ApiUrl)
+    return response
+
+
+@app.get("/logout", response_class=HTMLResponse)
+async def root(request: Request):
+    response = RedirectResponse('/')
+    response.delete_cookie('ApiUrl')
+    return response
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-
-
-
-
-
-
-from outline_vpn.outline_vpn import OutlineVPN
-
-# Setup the access with the API URL (Use the one provided to you after the server setup)
-client = OutlineVPN(api_url="")
-
-# Get all access URLs on the server
-for key in client.get_keys():
-    print(f'{key.name} {key.access_url}')
-#
-# # Create a new key
-# new_key = client.create_key()
-#
-# # Rename it
-# client.rename_key(new_key.key_id, "new_key")
-#
-# # Delete it
-# client.delete_key(new_key.key_id)
-#
-# # Set a monthly data limit for a key (20MB)
-# client.add_data_limit(new_key.key_id, 1024 * 1024 * 20)
-#
-# # Remove the data limit
-# client.delete_data_limit(new_key.key_id)
